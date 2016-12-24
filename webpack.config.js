c89a11f1e9b4bef69c2cc0c869ae0b2ce4a4a323
Tim Handy <tjhandy@gmail.com>
2016-12-24 12:12:29 +0000

    prep for Github pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/app.jsx',
     output: {
-        path: 'dist', 
+        path: 'docs', 
         filename: 'bundle.js'
     }, 
     resolve: {
